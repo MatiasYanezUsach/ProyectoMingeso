74158b52fdf6129985cdf27948a1682f9093cec9
Matias Yanez <matias.yanez@usach.cl>
2022-09-14 12:18:17 -0300

    inicializacion proyecto

---
+++ muebles_stgo/src/test/java/proyecto/mingeso/muebles_stgo/MueblesStgoApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.muebles_stgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesStgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
