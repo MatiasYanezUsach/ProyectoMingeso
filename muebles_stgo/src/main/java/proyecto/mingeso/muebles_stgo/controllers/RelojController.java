7f54d69530adb8af622f3a51d728bd5079ee96da
Matias Yanez <matias.yanez@usach.cl>
2022-09-29 20:50:15 -0300

    Mejoras en las vistas

--- muebles_stgo/src/main/java/proyecto/mingeso/muebles_stgo/controllers/RelojController.java
+++ muebles_stgo/src/main/java/proyecto/mingeso/muebles_stgo/controllers/RelojController.java
@@ -1,25 +1,24 @@
 package proyecto.mingeso.muebles_stgo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import proyecto.mingeso.muebles_stgo.entities.RelojEntity;
 import proyecto.mingeso.muebles_stgo.services.RelojService;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 public class RelojController {
 
     @Autowired
     private RelojService relojService;
 
-    @RequestMapping(value = "/marcas", method = RequestMethod.GET)
-    public ResponseEntity<ArrayList<RelojEntity>> obtenerMarcas() {
+    @GetMapping("/registroMarcas")
+    public String obtenerMarcas(Model model) {
         ArrayList<RelojEntity> marcas = relojService.obtenerMarcas();
-        return new ResponseEntity<>(marcas, HttpStatus.OK);
+        model.addAttribute("marcas",marcas);
+        return "RelojMarcas";
     }
 }
